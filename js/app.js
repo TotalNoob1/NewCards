9d63f5b8edb0e62ef91be33c201ad528915c25bf
Edward V Cruz <edwardcruzcoding@gmail.com>
2018-08-22 16:52:22 -0500

    fix: Fixed a bug that allowed the card to be clicked twice

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
- const icons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor","fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond","fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle",
- "fa-paper-plane-o", "fa-cube"];
+const icons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle",
+    "fa-paper-plane-o", "fa-cube"
+];
 
 /*
  * Display the cards on the page
@@ -15,10 +16,11 @@
 
 let moves = document.getElementsByClassName('moves');
 let clicks = 0;
-var children = $(".card").children().removeClass(icons);//removes all the icons from the cards
+var children = $(".card").children().removeClass(icons); //removes all the icons from the cards
 let clickedcards = [];
 let matchedcards = [];
-let star =  document.getElementsByClassName('fa-star');
+let star = document.getElementsByClassName('fa-star');
+let stars = 3;
 let bugfixer = 0;
 let minutes = 0;
 let ones = 0;
@@ -27,51 +29,49 @@ let clock = $(".clock")
 var time = 0;
 shuffle(icons);
 
-function timer(){
-  setInterval(function () {
-    ones = ones + 1;
-    if (ones == 10){
-      tens = tens +1;
-      ones = 0;
-    }
-    if (tens == 6){
-      tens = 0;
-      minutes = minutes + 1;
-    }
-    ones = ones.toString();
-    tens = tens.toString();
-    minutes = minutes.toString();
+function timer() {
+    setInterval(function() {
+        ones = ones + 1;
+        if (ones == 10) {
+            tens = tens + 1;
+            ones = 0;
+        }
+        if (tens == 6) {
+            tens = 0;
+            minutes = minutes + 1;
+        }
+        ones = ones.toString();
+        tens = tens.toString();
+        minutes = minutes.toString();
 
-    time = minutes.concat(":",tens,ones)
+        time = minutes.concat(":", tens, ones)
 
-    clock[0].innerHTML = time;
+        clock[0].innerHTML = time;
 
-    ones = Number(ones);
-    tens = Number(tens);
-    minutes = Number(minutes);
-
-
-  }, 1000);
-
-  console.log(clock[0].innerHTML);
+        ones = Number(ones);
+        tens = Number(tens);
+        minutes = Number(minutes);
+    }, 1000);
 }
 
 function clickshuffle() {
-  for (x = 0; x < icons.length;x++){
-    children[x].classList.add(icons[x]);
-  }
+    for (x = 0; x < icons.length; x++) {
+        children[x].classList.add(icons[x]);
+    }
 }
 $(".restart").click(function functionName() {
-  $(".card").children().removeClass(icons,'show open check match');
-  $(".card").removeClass('show open check match')
-  shuffle(icons);
-  clickshuffle();
-  moves[0].innerHTML = 0;
-  clicks = 0;
+    $(".card").children().removeClass(icons, 'show open check match');
+    $(".card").removeClass('show open check match')
+    shuffle(icons);
+    clickshuffle();
+    moves[0].innerHTML = 0;
+    clicks = 0;
 });
 clickshuffle();
+
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -83,63 +83,72 @@ function shuffle(array) {
 
     return array;
 }
+
 function mismatch() {
     var two = document.getElementsByClassName('check');
-    for (x = 0; x < two.length; x++){
-      two[0].classList.remove('show','open','check');
-      two[0].classList.remove('show','open','check');
+    for (x = 0; x < two.length; x++) {
+        two[0].classList.remove('show', 'open', 'check');
+        two[0].classList.remove('show', 'open', 'check');
     }
 }
-$(".deck").click(function(){
-  if( event.target.nodeName === 'LI')//check to make sure it a card
-  {
-    clickedcards.push(event.target);//add the element to the array to keep track of the amount of cards clicked
-    event.target.classList.add('show','open','check');
-    clicks = clicks + 1;
-    moves[0].innerHTML = clicks;//tracks the amount of clicks
-    if(clickedcards.length == 2){
-      if (clickedcards[0].innerHTML == clickedcards[1].innerHTML){
-        for (x = 0; x < clickedcards.length; x++){
-          clickedcards[x].classList.add('match');
-          clickedcards[x].classList.remove('check');
-          matchedcards.push(clickedcards[x]);
-      }
-      }else {
-        setTimeout(function(){mismatch();}, 500);
+$(".deck").click(function() {
+    if (event.target.classList.value != "card show open check") {
+        if (event.target.nodeName === 'LI') //check to make sure it a card
+        {
+            clickedcards.push(event.target); //add the element to the array to keep track of the amount of cards clicked
+            event.target.classList.add('show', 'open', 'check');
+            clicks = clicks + 1;
+            moves[0].innerHTML = clicks; //tracks the amount of clicks
+            if (clickedcards.length == 2) {
+                if (clickedcards[0].innerHTML == clickedcards[1].innerHTML) {
+                    for (x = 0; x < clickedcards.length; x++) {
+                        clickedcards[x].classList.add('match');
+                        clickedcards[x].classList.remove('check');
+                        matchedcards.push(clickedcards[x]);
+                    }
+                } else {
+                    setTimeout(function() {
+                        mismatch();
+                    }, 500);
+                }
+                clickedcards.pop();
+                clickedcards.pop();
+            }
+        }
+        if (matchedcards.length == 16) {
+            setTimeout(function() { //checks if you won
+                alert("Congrats You Won! It took you " + clock[0].innerHTML + " to win! You got " + stars + " stars");
+                for (x = 0; x < 40; x++) {
+                    matchedcards.pop
+                }
+            }, 1000);
+        }
+        if (bugfixer == 0) {
+            if (clicks > 20) {
+                star[2].classList.remove("fa-star");
+                bugfixer = bugfixer + 1;
+                stars = stars - 1;
+            }
+        }
+        if (bugfixer == 1) {
+            if (clicks > 30) {
+                star[1].classList.remove("fa-star");
+                bugfixer = bugfixer + 1;
+                stars = stars - 1;
+            }
+        }
+        if (bugfixer == 2) {
+            if (clicks > 50) {
+                star[0].classList.remove("fa-star");
+                bugfixer = bugfixer + 1;
+                stars = stars - 1;
+            }
+        }
     }
-      clickedcards.pop();
-      clickedcards.pop();
-    }
-  }
-  if(matchedcards.length == 16){
-    setTimeout(function() {//checks if you won
-      alert("Congrats You Won");
-      for (x = 0; x <40; x++){
-        matchedcards.pop
-      }
-    }, 1000);
-  }
-  if(bugfixer == 0){
-    if (clicks > 10){
-      star[2].classList.remove("fa-star");
-      bugfixer = bugfixer + 1;
-    }
-  }
-  if (bugfixer == 1){
-    if(clicks > 20){
-      star[1].classList.remove("fa-star");
-      bugfixer = bugfixer + 1;
-    }
-  }
-  if(bugfixer == 2){
-    if (clicks > 30){
-      star[0].classList.remove("fa-star");
-      bugfixer = bugfixer + 1;
-    }
-  }
 });
-$(".start").click(function functionName() {
-  timer();
+$(".start").click(function time() {
+    timer();
+    $(".start").off("click", time);
 });
 /*
  * set up the event listener for a card. If a card is clicked:
