02a023dc6cb6523a4ade68587552550b739a4718
Edward V Cruz <edwardcruzcoding@gmail.com>
2018-08-25 19:25:44 -0500

    Feat: Added a win screen

--- js/app.js
+++ js/app.js
@@ -20,14 +20,19 @@ var children = $(".card").children().removeClass(icons); //removes all the icons
 let clickedcards = [];
 let matchedcards = [];
 let star = document.getElementsByClassName('fa-star');
-let stars = 3;
+let stars = 5;
+let newstar = $(".star")
 let bugfixer = 0;
 let minutes = 0;
 let ones = 0;
 let tens = 0;
 let clock = $(".clock")
 var time = 0;
+const score = $(".score-panel")
 let newTime;
+const win = $(".win");
+const game = $(".container");
+let wins = 0;
 /*<------------------------------------------------------------------------------THIS IS A DIVIDER--------------------------------------------------------------------------------------------------------------->*/
 shuffle(icons);
 /*<------------------------------------------------------------------------------THIS IS A DIVIDER--------------------------------------------------------------------------------------------------------------->*/
@@ -51,6 +56,7 @@ function resetTime() {
         time = minutes.concat(":", tens, ones)
 
         clock[0].innerHTML = time;
+        clock[1].innerHTML = time;
 
         ones = Number(ones);
         tens = Number(tens);
@@ -69,15 +75,38 @@ $(".restart").click(function functionName() {
     $(".card").removeClass('show open check match')
     shuffle(icons);
     clickshuffle();
-    moves[0].innerHTML = 0;
     clicks = 0;
-    clock[0].innerHTML = "0:00";
     ones = 0;
     tens = 0;
     minutes = 0;
     clearInterval(newTime);
     clickedcards.pop();
     clickedcards.pop();
+    for (x = 0; x  < 2; x++){
+      moves[x].innerHTML = 0;
+      clock[x].innerHTML = "0:00";
+    }
+    if (clicks >= 20){
+    if (1 == bugfixer){
+      newstar[5].classList.add("fa-star");
+      newstar[2].classList.add("fa-star");
+      console.log(123);
+    }
+    if (2 == bugfixer){
+      newstar[4].classList.add("fa-star");
+      newstar[1].classList.add("fa-star");
+    }
+    if (3 == bugfixer){
+      newstar[3].classList.add("fa-star");
+      newstar[0].classList.add("fa-star");
+
+    }
+    }
+    if (wins == 1){
+    win[0].style.display = "none";
+    game[0].style.display = "flex";
+    wins = wins - 1;
+  }
     $(".start").click(function time() {
         resetTime()
         $(".start").off("click", time);
@@ -115,7 +144,8 @@ $(".deck").click(function() {
             clickedcards.push(event.target); //add the element to the array to keep track of the amount of cards clicked
             event.target.classList.add('show', 'open', 'check');
             clicks = clicks + 1;
-            moves[0].innerHTML = clicks; //tracks the amount of clicks
+            moves[0].innerHTML = clicks;
+            moves[1].innerHTML = clicks; //tracks the amount of clicks
             if (clickedcards.length == 2) {
                 if (clickedcards[0].innerHTML == clickedcards[1].innerHTML) {
                     for (x = 0; x < clickedcards.length; x++) {
@@ -138,31 +168,37 @@ $(".deck").click(function() {
         }
         if (matchedcards.length == 16) {
             setTimeout(function() { //checks if you won
-                alert("Congrats You Won! It took you " + clock[0].innerHTML + " to win! You got " + stars + " stars");
+                win[0].style.display = "flex";
+                game[0].style.display = "none";
+                clearInterval(newTime);
+                wins = wins + 1;
                 for (x = 0; x < 40; x++) {
-                    matchedcards.pop
+                    matchedcards.pop();
                 }
             }, 1000);
         }
         if (bugfixer == 0) //Makes it so that the same if statement doesn't get run twice {
             if (clicks > 20) {
+                star[5].classList.remove("fa-star");
                 star[2].classList.remove("fa-star");
                 bugfixer = bugfixer + 1;
-                stars = stars - 1;
+                stars = stars + 2;
             }
     }
     if (bugfixer == 1) {
-        if (clicks > 30) {
+        if (clicks > 35) {
+            star[3].classList.remove("fa-star");
             star[1].classList.remove("fa-star");
             bugfixer = bugfixer + 1;
-            stars = stars - 1;
+            stars = stars + 2;
         }
     }
     if (bugfixer == 2) {
         if (clicks > 50) {
+            star[1].classList.remove("fa-star");
             star[0].classList.remove("fa-star");
             bugfixer = bugfixer + 1;
-            stars = stars - 1;
+            stars = stars + 2;
         }
     }
 
@@ -172,6 +208,11 @@ $(".start").click(function time() {
     resetTime();
     $(".start").off("click", time);
 });
+
+
+
+
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
